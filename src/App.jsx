bf0654854b2366b48b1dd2e69ac021b0fe29d1c3
Alwin Sunil <64636626+AlwinSunil@users.noreply.github.com>
2023-12-06 12:33:20 +0530

    Implemented stage 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import "./App.css";
+import { Component } from "react";
 import Home from "./components/Home/Home";
 import Quiz from "./components/Quiz/Quiz";
 import Result from "./components/Result/Result";
+import questions from "./data/quizQuestion.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="app">
-      <Home />
-      <Quiz />
-      <Result />
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currQues: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Home />
+        <Quiz
+          state={this}
+          question={questions[this.state.currQues]}
+          index={this.state.currQues}
+          length={questions.length}
+        />
+        <Result />
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -3,24 +3,53 @@ import "./Quiz.css";
 
 export default class Quiz extends Component {
   render() {
+    const { state, question, index, length } = this.props;
+
     return (
       <div className="quiz">
         <div className="container">
           <h2>Question</h2>
           <p className="track">
-            <span>1</span> of <span>15</span>
+            <span>{index + 1}</span> of <span>{length}</span>
           </p>
-          <span className="ques">Which is the only mammal that can jump?</span>
+          <span className="ques">{question.question}</span>
           <div className="options">
-            <div className="option">Dog</div>
-            <div className="option">Elephant</div>
-            <div className="option">Goat</div>
-            <div className="option">Lion</div>
+            <div className="option">{question.optionA}</div>
+            <div className="option">{question.optionB}</div>
+            <div className="option">{question.optionC}</div>
+            <div className="option">{question.optionD}</div>
           </div>
           <div className="action">
-            <button className="prev">Previous</button>
-            <button className="next">Next</button>
-            <button className="quit">Quit</button>
+            <button
+              className="prev"
+              onClick={() => {
+                state.setState({
+                  currQues: (state.state.currQues - 1 + length) % length,
+                });
+              }}
+            >
+              Previous
+            </button>
+            <button
+              className="next"
+              onClick={() => {
+                state.setState({
+                  currQues: (state.state.currQues + 1) % length,
+                });
+              }}
+            >
+              Next
+            </button>
+            <button
+              className="quit"
+              onClick={() => {
+                if (window.confirm("Are you sure you want to quit ?"))
+                  console.log("Quit");
+                else console.log("Continue your quiz");
+              }}
+            >
+              Quit
+            </button>
           </div>
         </div>
       </div>
